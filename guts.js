0e99c1a4fcca81325c493c647e3d76d464e2e335
EDLyonhart <EDLyonhart@gmail.com>
2015-01-18 21:56:05 -0800

    Working board, reset button

--- guts.js
+++ guts.js
@@ -1,47 +1,81 @@
-//listen for click.
-//determin whos turn it is
-	//is class 'unUsedSquare'?
-		//change background on click based on player
-		//change class to usedSquare
-		//progress the turn count
 
-window.onload = beginning;
+window.addEventListener("load", function () {
 
-function beginning() {
+	var turn = 0;
+	var squares = document.getElementsByClassName("unUsedSquare");
+	var winningCondition = false;
+
+
+	//cycle through squares apply click
+		//dont apply if square is already clicked <-------------- still to do
+			//.removeEventListener
+			//if this.innerHtml === ""
+
+	function clickedSquare () {
+		for (i = 0; i < squares.length; i++) {
+			squares[i].addEventListener("click", addMarker);
+		}
+	}
 	clickedSquare();
-}
 
-var turn = 0;
-var squares = document.getElementsByClassName("unUsedSquare");
+	//change background and marker based on which player is up
 
-
-//cycle through squares apply click
-
-function clickedSquare () {
-	for (i = 0; i < squares.length; i++) {
-		squares[i].addEventListener("click", addMarker);
+	function addMarker () {
+		if (turn % 2 === 0 && winningCondition === false) {
+			this.style.backgroundColor="red";
+			this.innerHTML="X";
+		} else {
+			this.style.backgroundColor="yellow";
+			this.innerHTML="O";
+		}
+		turn++;
 	}
-}
-clickedSquare();
 
-//determin if square is an unUsedSquare
-//acknowledge who's turn it is
-	//change background
-	//add 'x'/'o'
-	//change class name to 'usedSquare'
 
-function addMarker () {
-	console.log("functioning");
-	if (turn % 2 === 0) {
-		this.style.backgroundColor="red";
-		this.innerHTML="X";
-	} else {
-		this.style.backgroundColor="yellow";
-		this.innerHTML="O";
+	//create a restart button
+	//listen for click
+	//cycle through squares and change info back to beginning
+
+	function clearBoard(event) {
+		for (i = 0; i < squares.length; i++) {
+			squares[i].className = "col-sm-2 unUsedSquare";
+			squares[i].style.backgroundColor= "white";
+			squares[i].innerHTML="";
+		}
 	}
-	event.target.className = "col-sm-2 usedSquare";
-	turn++;
-}
+
+	var resetButton = document.getElementsByClassName("reset");
+	resetButton[0].addEventListener("click", clearBoard);
+});
+
+// switch (winningCondition) { 
+// 	case (document.getElementById("1") && (document.getElementById("2") && (document.getElementById("3"))
+// 		winningCondition = true;
+// 		break;
+// 	case (document.getElementById("4") && (document.getElementById("5") && (document.getElementById("6")):
+// 		winningCondition = true;
+// 		break;
+// 	case (document.getElementById("7") && (document.getElementById("8") && (document.getElementById("9")):
+// 		winningCondition = true;
+// 		break;
+// 	case (document.getElementById("1") && (document.getElementById("4") && (document.getElementById("7")):
+// 		winningCondition = true;
+// 		break;
+// 	case (document.getElementById("2") && (document.getElementById("5") && (document.getElementById("8")):
+// 		winningCondition = true;
+// 		break;
+// 	case (document.getElementById("3") && (document.getElementById("6") && (document.getElementById("9")):
+// 		winningCondition = true;
+// 		break;
+// 	case (document.getElementById("1") && (document.getElementById("5") && (document.getElementById("9")):
+// 		winningCondition = true;
+// 		break;
+// 	case (document.getElementById("3") && (document.getElementById("5") && (document.getElementById("7")):
+// 		winningCondition = true;
+// 		break;
+// 	default: winningCondition = false;
+// 		{
+// }
 
 
 
