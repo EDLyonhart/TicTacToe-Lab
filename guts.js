21a3c6a3efa6fbd6a477d8cb3f531036dcf11d44
EDLyonhart <EDLyonhart@gmail.com>
2015-01-20 09:22:35 -0800

    first bonus

--- guts.js
+++ guts.js
@@ -1,13 +1,10 @@
 
 window.addEventListener("load", function () {
-
 	var turn = 0;
 	var squares = document.getElementsByClassName("unUsedSquare");
 	var winningCondition = false;
 
-
 	//cycle through squares apply click
-
 	function clickedSquare () {
 			for (i = 0; i < squares.length; i++) {
 				squares[i].addEventListener("click", addMarker);
@@ -15,28 +12,27 @@ window.addEventListener("load", function () {
 		}
 		clickedSquare();
 
-	//change background and marker based on which player is up
-		//recognize if square has already been clicked based on innerHTML
 
+	//change background, message and marker based on which player is up
+		//recognize if square has already been clicked based on innerHTML
 	function addMarker () {
 		if (this.innerHTML != "X" && this.innerHTML != "O") {
-			console.log("working");
 			if (turn % 2 === 0 && winningCondition === false) {
-				this.style.backgroundColor="red";
+				this.style.backgroundColor="Gold";
 				this.innerHTML="X";
+				document.getElementById("message").innerHTML = "'X's turn";
 			} else {
-				this.style.backgroundColor="yellow";
+				this.style.backgroundColor="IndianRed";
 				this.innerHTML="O";
+				document.getElementById("message").innerHTML = "'O's turn";
 			}
 			turn++;
 		}
 	}
 
-
 	//create a restart button
 	//listen for click
 	//cycle through squares and change info back to beginning
-
 	function clearBoard(event) {
 		for (i = 0; i < squares.length; i++) {
 			squares[i].className = "col-sm-2 unUsedSquare";
@@ -50,7 +46,7 @@ window.addEventListener("load", function () {
 });
 
 
-
+[1,2,3], [4,5,6], [7,8,9], [1,5,9], [2,5,8], [3,5,7], [1,4,7], [2,5,8], [3,6,9]
 
 
 
